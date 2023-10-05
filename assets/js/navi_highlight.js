31822b0e45161ce59e34a30117712dab494bf0d0
raphael-s-steiner <raphaelsteiner@gmail.com>
2023-10-05 13:15:14 +0200

    nav highlight

---
+++ assets/js/navi_highlight.js
@@ -0,0 +1,31 @@
+(function () {
+  'use strict';
+  // constants
+  const sections = [...document.querySelectorAll('section[id]')];
+  const menuLinks = [...document.querySelectorAll('.nav-item a')];
+  const navoffset = document.getElementById('mainNav').clientHeight;
+  const navToggle = document.querySelector('.navbar-toggler');
+  // Scrollspy handler
+  const scrollHandle = () => {
+    const scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+    menuLinks.forEach((link) => link.classList.remove('active'));
+    sections.forEach((section) => {
+      const offset = section.offsetTop - navoffset;
+      if (offset <= scrollPosition) {
+        menuLinks.forEach((link) => link.classList.remove('active'));
+        const target = document.querySelector(`a[id="nav-bar-${section.id}"]`);
+        if (!target.hash) return;
+        target.classList.add('active');
+        if (target.parentElement.classList.contains('dropdown-menu')) {
+          target.parentElement.parentElement.firstChild.classList.add('active');
+        }
+        if (document.querySelector('#navbarResponsive').classList.contains('show')) navToggle.click();
+      }
+    });
+  };
+
+  // event listeners
+  document.addEventListener('scroll', scrollHandle, false);
+  document.addEventListener('resize', scrollHandle, false);
+  document.addEventListener('readystatechange', scrollHandle, false);
+})();
