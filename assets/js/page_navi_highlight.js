48b65c8c01977318234f86d401f11af25377d90c
raphael-s-steiner <raphaelsteiner@gmail.com>
2023-10-06 10:38:30 +0200

    publication restructure 4

--- assets/js/page_navi_highlight.js
+++ assets/js/page_navi_highlight.js
@@ -1,3 +1,5 @@
 var page_stripped = window.location.href.replace("{{ site.url }}", "").replace("/", "").split("/");
+console.log(page_stripped[0])
 var element = document.getElementById("nav-bar-"+page_stripped[0]);
+console.log(element)
 element.classList.add("page-nav-highlight");
