3cd48b86218fc45135efa36b20a9bc55be499d0e
raphael-s-steiner <raphaelsteiner@gmail.com>
2023-10-06 11:22:27 +0200

    publication restructure 8

--- assets/js/page_navi_highlight.js
+++ assets/js/page_navi_highlight.js
@@ -3,4 +3,8 @@ var page_stripped = window.location.href.split("/");
 //console.log(page_stripped);
 var element = document.getElementById("nav-bar-"+page_stripped[3]);
 //console.log(element);
-element.classList.add("page-nav-highlight");
+if(element !== null){
+  element.classList.add("page-nav-highlight");
+} else{
+  console.log("The suburl string is null")
+}
